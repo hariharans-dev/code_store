c7e30d5458d03534deb01e704bc37ef4e6961f30
hariharan <shariharan182003@gmail.com>
2022-09-25 15:19:21 +0530

    commit

--- java/pack/calculator.java
+++ java/pack/calculator.java
@@ -1,6 +1,9 @@
 package pack;
 public class calculator {
     public void addition(int a[]){
-        
+        int s=0,n=a.length;
+        for(int i=0;i<n;i++)
+            s+=a[i];
+        System.out.println(s);
     }
 }
