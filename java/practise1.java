e8665ce2a80c5d23975a555169b48bb65880aa31
shariharan182003 <94595625+shariharan182003@users.noreply.github.com>
2022-10-03 10:29:50 +0530

    C1

--- java/practise1.java
+++ java/practise1.java
@@ -1,17 +1,4 @@
 import java.util.*;
 public class practise1{
-    public static void main(String args[]){
-        child2 c=new child2();
-        c.hello1();
-    }
-}
-class child1 extends practise1{
-    void hello1(){
-        System.out.println("child1");
-    }
-}
-class child2 extends child1{
-    void hello(){
-        System.out.println("child2");
-    }
+    sys
 }
