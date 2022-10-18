34af0f429c75d860c872e01509ef9a329ff259c8
shariharan182003 <94595625+shariharan182003@users.noreply.github.com>
2022-10-18 23:53:22 +0530

    cm1

--- c and c++/daproject_store.cpp
+++ c and c++/daproject_store.cpp
@@ -42,3 +42,56 @@ int main()
 
 //question 2
 
+#include <iostream>
+#include <conio.h>
+#include<string.h>
+using namespace std;
+struct stack
+{
+	char s[20];
+	int top;
+	stack()
+	{
+		top=-1;
+	}
+}st;
+int push(char c)
+{
+	st.s[++st.top] = c;
+}
+char pop()
+{
+	char e=st.s[st.top];
+	st.top--;
+	return e;
+}
+int main() 
+{
+	int n;cin>>n;
+	while(n>=0)
+	{
+		string s;
+		getline(cin,s);
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			if(s[i]!=' ')
+				push(s[i]);
+			else 
+			{
+				while(st.top!=-1)
+					cout<<pop();
+				cout<<" ";
+			}
+		}
+		while(st.top!=-1)
+			cout<<pop();
+		n--;
+	}
+    getch();
+	return 0;
+}
+
+// question 3
+
+
+
--- c and c++/practise2.cpp
+++ c and c++/practise2.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 #include <conio.h>
+#include<string.h>
 using namespace std;
-int main() {
-	int n;cin>>n;
-	for(int i=0;i<n;i++)
+struct stack
+{
+	char s[20];
+	int top;
+	stack()
 	{
-	    int a,b,x,y;
-	    cin>>a>>b>>x>>y;
-	    float t1=a/x,t2=b/y;
-        if(t1==t2)
-            cout<<"Both"<<endl;
-        else if(t1>t2)
-            cout<<"Chef"<<endl;
-        else 
-            cout<<"Chefina"<<endl;
+		top=-1;
+	}
+}st;
+int push(char c)
+{
+	st.s[++st.top] = c;
+}
+char pop()
+{
+	char e=st.s[st.top];
+	st.top--;
+	return e;
+}
+int main() 
+{
+	int n;cin>>n;
+	while(n>=0)
+	{
+		string s;
+		getline(cin,s);
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			if(s[i]!=' ')
+				push(s[i]);
+			else 
+			{
+				while(st.top!=-1)
+					cout<<pop();
+				cout<<" ";
+			}
+		}
+		while(st.top!=-1)
+			cout<<pop();
+		n--;
 	}
     getch();
 	return 0;
