0e9a18b647c0c4e416eb6108218777465241e42d
hari <shariharan182003@gmail.com>
2022-12-30 11:37:36 +0530

    javascript addition

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+let b=1;
+function oc(a){
+    b=a;
+};
+document.getElementById('h3').innerHTML=b;
---
+++ javascript/script1.js
@@ -0,0 +1,3 @@
+document.getElementById("h2").innerHTML=5;
+// window.alert('haran');
+document.write('hari')
